d356b4d87d19683e7cf9331d5e22b769a00d015b
Andanchenko Maria <andanchenko.maria@gmail.com>
2024-08-10 15:21:27 +0300

    data

---
+++ script.sql
@@ -0,0 +1,24 @@
+/*1*/
+select orders.id, 
+orders.date, 
+YEAR(orders.date) as YEAR, 
+MONTH(orders.date) as MONTH, 
+DAY(orders.date) as DAY
+from orders;
+/*2*/
+select orders.id, 
+orders.date,
+DATE_ADD(orders.date, INTERVAL 1 DAY) as new_date
+from orders;
+ 
+/*3*/
+select orders.id, 
+orders.date, 
+UNIX_TIMESTAMP(orders.date)
+from orders;
+/*4*/
+select count(orders.date)
+from orders
+where orders.date between '1996-07-10 00:00:00' and '1996-10-08 00:00:00'
+
+/*5*/
