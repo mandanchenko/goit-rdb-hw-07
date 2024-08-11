9741a5881ba2d1fc1ac9fb111825f42a8e72fc1e
Andanchenko Maria <andanchenko.maria@gmail.com>
2024-08-11 22:39:25 +0300

    fin

--- script.sql
+++ script.sql
@@ -19,6 +19,11 @@ from orders;
 /*4*/
 select count(orders.date)
 from orders
-where orders.date between '1996-07-10 00:00:00' and '1996-10-08 00:00:00'
+where orders.date between '1996-07-10 00:00:00' and '1996-10-08 00:00:00';
 
 /*5*/
+SELECT 
+    id,
+    date,
+    JSON_OBJECT('id', id, 'date', date) AS order_json
+FROM orders;
